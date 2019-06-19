f6034b63e6ca8be0611c30f77e39558d1fed56f2
jhonnrock <jhonn7890@gmail.com>
2019-06-19 16:18:47 -0400

    List of dto and model

---
+++ src/main/java/edu/umss/storeservice/model/User.java
@@ -0,0 +1,57 @@
+/**
+ * @author: Johnny .
+ */
+
+package edu.umss.storeservice.model;
+
+
+import javax.persistence.Entity;
+
+@Entity
+public class User extends ModelBase {
+    private String Account_user;
+    private Byte Password_User;
+    private Byte RePassword_User;
+    private String Security_Quest1;
+    private String Security_Quest2;
+
+    public String getAccount_user() {
+        return Account_user;
+    }
+
+    public void setAccount_user(String account_user) {
+        Account_user = account_user;
+    }
+
+    public Byte getPassword_User() {
+        return Password_User;
+    }
+
+    public void setPassword_User(Byte password_User) {
+        Password_User = password_User;
+    }
+
+    public Byte getRePassword_User() {
+        return RePassword_User;
+    }
+
+    public void setRePassword_User(Byte rePassword_User) {
+        RePassword_User = rePassword_User;
+    }
+
+    public String getSecurity_Quest1() {
+        return Security_Quest1;
+    }
+
+    public void setSecurity_Quest1(String security_Quest1) {
+        Security_Quest1 = security_Quest1;
+    }
+
+    public String getSecurity_Quest2() {
+        return Security_Quest2;
+    }
+
+    public void setSecurity_Quest2(String security_Quest2) {
+        Security_Quest2 = security_Quest2;
+    }
+}
